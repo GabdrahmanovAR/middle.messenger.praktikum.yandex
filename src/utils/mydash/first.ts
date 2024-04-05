fe184f373e47e230ac2555cb097e1ceedad0c48b
Grosno <pbc20092yandex.ru>
2024-04-06 01:20:11 +0300

    fix: исправление ошибки теста 'Check newline at end of files'

--- src/utils/mydash/first.ts
+++
@@ -1,8 +0,0 @@
-export function first(list: unknown) {
-  if (!Array.isArray(list)) {
-    return undefined;
-  }
-
-  const length = list.length;
-  return length ? list[0] : undefined;
-}
--- src/utils/mydash/last.ts
+++
@@ -1,8 +0,0 @@
-export function last(list: unknown) {
-  if (!Array.isArray(list)) {
-    return undefined;
-  }
-
-  const length = list.length;
-  return length ? list[length - 1] : undefined;
-}
