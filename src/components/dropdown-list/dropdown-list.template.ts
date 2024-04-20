8316e238595c0e9290975331946edf24968e8a12
Grosno <pbc20092yandex.ru>
2024-04-21 00:19:51 +0300

    Исправлена ошибка newline at end of files

---
+++ src/components/dropdown-list/dropdown-list.template.ts
@@ -0,0 +1,5 @@
+import { EMPTY_STRING } from '../../../assets/constants/common';
+
+const dropdownListTemplate = EMPTY_STRING;
+
+export default dropdownListTemplate;
---
+++ src/components/message-list/message-list.template.ts
@@ -0,0 +1,5 @@
+import { EMPTY_STRING } from '../../../assets/constants/common';
+
+const messageList = EMPTY_STRING;
+
+export default messageList;
